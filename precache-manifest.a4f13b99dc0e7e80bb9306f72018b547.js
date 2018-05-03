3a2b3d39b057311ba81ff08fd3b61699901c6eb5
Gautam Naik <gautamnaik1994@gmail.com>
2018-05-04 01:04:16 +0530

    Updates

---
+++ precache-manifest.a4f13b99dc0e7e80bb9306f72018b547.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "13788957135da63bb089",
+    "url": "vendors.13788957.js"
+  },
+  {
+    "revision": "dd567b22dfceee7737f4",
+    "url": "main.dd567b22.js"
+  },
+  {
+    "revision": "dd567b22dfceee7737f4",
+    "url": "main.dd567b22.css"
+  },
+  {
+    "revision": "fe57491da743e6419174a2b527551821",
+    "url": "index.html"
+  }
+];
