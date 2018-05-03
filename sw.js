19109144b91f9e3384b5307cdd1757894292a567
Gautam Naik <gautamnaik1994@gmail.com>
2018-05-04 01:25:38 +0530

    Updates

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c0b7d90f4b059f199a0203ff0b1d77b6.js"
+  "precache-manifest.651dea5a824115df0cea6e24df6ec613.js"
 );
 
 workbox.skipWaiting();
